19bbcbeb0e15484e838056f944c097c424cec58e
Joshua Dada <joshuadada1996@gmail.com>
2023-12-14 01:43:21 +0000

    updated react code
    yessir

--- 03-pizza-menu/starter/src/index.js
+++ 03-pizza-menu/starter/src/index.js
@@ -4,6 +4,17 @@ import ReactDom from "react-dom/client";
 function App() {
   return <h1>Hello React</h1>;
 }
+// React v18 - how we render the root in v18
+//Strict mode makes makes it so it renders the components twice during development
+// it renders twice to find bugs
+//It also checks if we are using outdated parts of the react API
 
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+//this is how we use to render root - React Before v18
+//React.render(<App />;)
